6e52e0ec8abf04098b533b9a3c46eb275683d646
Ludovic Claude <ludovic.claude@laposte.net>
2014-04-15 02:07:43 +0200

    Add images for Scala, sbt and play

--- bin/install.sh
+++ bin/install.sh
@@ -1,38 +1,12 @@
 #!/bin/bash
+# options:
+# --no-update : skip updating the Docker images from Internet
 
-REGISTRY=internal_registry:5000
-IMAGES_PATH=/vagrant/images
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Pull the external images
-/vagrant/bin/update.sh
-
-# Use latest tag 
-for i in $(eval echo "base appuser"); do
-  cd $IMAGES_PATH/$i
-  docker build -t="$REGISTRY/$i" .
-done;
-
-for i in $(eval echo "oracle-java7"); do
-  cd $IMAGES_PATH/$i
-  docker build -t="$REGISTRY/$i" .
-done;
-
-for i in $(eval echo "sbt"); do
-  cd $IMAGES_PATH/$i
-  docker build -t="$REGISTRY/$i" .
-done;
-
-for i in $(eval echo "riak-cs"); do
-  cd $IMAGES_PATH/$i
-  docker build -t="$REGISTRY/$i" .
-done;
-
-for i in $(eval echo "scala"); do
-  cd $IMAGES_PATH/$i
-  docker build -t="$REGISTRY/$i" .
-done;
-
-for i in $(eval echo "playframework"); do
-  cd $IMAGES_PATH/$i
-  docker build -t="$REGISTRY/$i" .
-done;
+cd $DIR/../ansible-playbooks
+if [ "$1" == "--no-update" ]; then
+  ansible-playbook -K -e master=true --skip-tags=update build.yml
+else
+  ansible-playbook -K -e master=true build.yml
+fi
