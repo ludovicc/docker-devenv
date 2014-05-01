db4782d7edb98276d8b91f8d753c22d19848fa17
Ludovic Claude <ludovic.claude@laposte.net>
2014-05-02 01:43:17 +0200

    Ubuntu 14.04, Scala 2.11

--- bin/install.sh
+++ bin/install.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 # options:
+# --clean     : do not use Docker cache when building, this will refresh everything
 # --no-update : skip updating the Docker images from Internet
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+OPTS="-e master=true"
+
+while :
+do
+    case $1 in
+        -h | --help | -\?)
+            #  Call your Help() or usage() function here.
+            echo "Usage: ./install.sh [--clean] [--no-update]"
+            echo "Options:"
+            echo "  -c or --clean or --no-cache : do not use Docker cache when building, this will refresh everything"
+            echo "  -N or --no-update : skip updating the Docker images from Internet"
+            exit 0      # This is not an error, User asked help. Don't do "exit 1"
+            ;;
+        -c | --clean | --no-cache)
+            OPTS="$OPTS -e docker_build_opts=--no-cache"
+            shift
+            ;;
+        -N | --no-update)
+            OPTS="$OPTS --skip-tags=update"
+            shift
+            ;;
+        --) # End of all options
+            shift
+            break
+            ;;
+        -*)
+            printf >&2 'WARN: Unknown option (ignored): %s\n' "$1"
+            shift
+            ;;
+        *)  # no more options. Stop while loop
+            break
+            ;;
+    esac
+done
 
 cd $DIR/../ansible-playbooks
-if [ "$1" == "--no-update" ]; then
-  ansible-playbook -K -e master=true --skip-tags=update build.yml
-else
-  ansible-playbook -K -e master=true build.yml
-fi
+ansible-playbook -K $OPTS build.yml
